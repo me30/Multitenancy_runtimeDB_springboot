13722dff7472cf83f935ab44639472769671a888
Java <meena@meena-Vostro-3549>
2019-11-01 15:31:12 +0530

    first code

---
+++ src/main/java/com/commons/MultitenancyRuntimeDbSpringbootApplication.java
@@ -0,0 +1,92 @@
+package com.commons;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.commons.core.TargetDataSources;
+import com.commons.routing.TenantAwareRoutingSource;
+import com.zaxxer.hikari.HikariDataSource;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class MultitenancyRuntimeDbSpringbootApplication extends SpringBootServletInitializer {
+
+	/*public static void main(String[] args) {
+		SpringApplication.run(MultitenancyRuntimeDbSpringbootApplication.class, args);
+	}*/
+	public static void main(String[] args) {
+		new MultitenancyRuntimeDbSpringbootApplication()
+				.configure(new SpringApplicationBuilder(MultitenancyRuntimeDbSpringbootApplication.class))
+				.properties(getDefaultProperties())
+				.run(args);
+	}
+	
+	@Value("${db.username}")
+	private String username;
+	
+	@Value("${db.password}")
+	private String password;
+	
+	@Value("${db.url.Tenant}")
+	private String urlTenant;
+	
+	@Value("${db.driverclass}")
+	private String driverclass;
+	
+	@Bean
+	public DataSource dataSource() {
+		AbstractRoutingDataSource dataSource = new TenantAwareRoutingSource();
+		/*Map<Object,Object> targetDataSources = new HashMap<>();
+		targetDataSources.put("TenantOne", tenantOne());
+		targetDataSources.put("TenantTwo", tenantTwo());
+		TargetDataSources.getTargetDataSources().put("TenantOne", tenantOne());
+		TargetDataSources.getTargetDataSources().put("TenantTwo", tenantTwo());*/
+		dataSource.setTargetDataSources(TargetDataSources.getTargetDataSources());
+		dataSource.afterPropertiesSet();
+		return dataSource;
+	}
+
+	/*public DataSource tenantOne() {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setInitializationFailTimeout(0);
+		dataSource.setMaximumPoolSize(5);
+		dataSource.setDataSourceClassName(driverclass);
+		dataSource.addDataSourceProperty("url", urlTenant+"sampledb");
+		dataSource.addDataSourceProperty("user", username);
+		dataSource.addDataSourceProperty("password", password);
+		return dataSource;
+	}
+
+	public DataSource tenantTwo() {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setInitializationFailTimeout(0);
+		dataSource.setMaximumPoolSize(5);
+		dataSource.setDataSourceClassName(driverclass);
+		dataSource.addDataSourceProperty("url", urlTenant+"sampledb2");
+		dataSource.addDataSourceProperty("user", username);
+		dataSource.addDataSourceProperty("password", password);
+		return dataSource;
+	}*/
+
+	private static Properties getDefaultProperties() {
+		Properties defaultProperties = new Properties();
+		// Set sane Spring Hibernate properties:
+		defaultProperties.put("spring.jpa.show-sql", "true");
+		defaultProperties.put("spring.jpa.hibernate.naming.physical-strategy", "org.hibernate.boot.model.naming.PhysicalNamingStrategyStandardImpl");
+		defaultProperties.put("spring.datasource.initialize", "false");
+		// Prevent JPA from trying to Auto Detect the Database:
+		defaultProperties.put("spring.jpa.database", "postgresql");
+		// Prevent Hibernate from Automatic Changes to the DDL Schema:
+		defaultProperties.put("spring.jpa.hibernate.ddl-auto", "none");
+		return defaultProperties;
+	}
+}
---
+++ src/main/java/com/commons/controller/TenantController.java
@@ -0,0 +1,115 @@
+package com.commons.controller;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.commons.core.TargetDataSources;
+import com.commons.model.TenantModel;
+import com.zaxxer.hikari.HikariDataSource;
+
+@RestController
+@RequestMapping(value="/tenant")
+public class TenantController {
+
+	@Autowired
+	AbstractRoutingDataSource dataSource;
+
+	@Value("${db.username}")
+	private String username;
+
+	@Value("${db.password}")
+	private String password;
+
+	@Value("${db.url.Tenant}")
+	private String urlTenant;
+
+	@Value("${db.driverclass}")
+	private String driverclass;
+
+	@PersistenceContext
+	EntityManager entityManager;
+
+	@RequestMapping(value="/create", method=RequestMethod.POST)
+	public ResponseEntity<?> create(@RequestBody TenantModel model) throws Exception{
+		createDatabase(model);
+		executeScriptUsingStatement(model);
+		
+		TargetDataSources.getTargetDataSources().put(model.getTenantID(), tenantRuntime(model.getDbname()));
+		dataSource.setTargetDataSources(TargetDataSources.getTargetDataSources());
+		dataSource.afterPropertiesSet(); 
+
+		return new ResponseEntity<String>("OK", HttpStatus.CREATED);
+	} 
+
+	private void createDatabase(TenantModel model) throws Exception {
+		Class.forName(driverclass);
+		Connection conn = DriverManager.getConnection(urlTenant, username, password);
+		Statement stmt = conn.createStatement();
+		stmt.executeUpdate("create database " + model.getDbname());
+
+		if (conn != null && !conn.isClosed()) {
+			conn.close();
+		}
+	}
+
+	private void executeScriptUsingStatement(TenantModel model) throws Exception {
+		String scriptFilePath = "create.sql";
+		BufferedReader reader = null;
+		Connection con = null;
+		Statement statement = null;
+		try {
+			// load driver class for mysql
+			Class.forName(driverclass);
+			// create connection
+			con = DriverManager.getConnection(urlTenant+model.getDbname(), username, password);
+			// create statement object
+			statement = con.createStatement();
+			// initialize file reader
+			reader = new BufferedReader(new FileReader(getClass().getClassLoader().getResource(scriptFilePath).getFile()));
+			String line = null;
+			// read script line by line
+			while ((line = reader.readLine()) != null) {
+				// execute query
+				statement.execute(line);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// close file reader
+			if (reader != null) {
+				reader.close();
+			}
+			// close db connection
+			if (con != null) {
+				con.close();
+			}
+		}
+	}
+
+	public DataSource tenantRuntime(String name) {
+		HikariDataSource dataSource = new HikariDataSource();
+		dataSource.setInitializationFailTimeout(0);
+		dataSource.setMaximumPoolSize(5);
+		dataSource.setDataSourceClassName(driverclass);
+		dataSource.addDataSourceProperty("url", urlTenant+name);
+		dataSource.addDataSourceProperty("user", username);
+		dataSource.addDataSourceProperty("password", password);
+		return dataSource;
+	}
+	
+}
